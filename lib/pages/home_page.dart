ab0a106752541b1b905a9e7d892373de9328036f
lodny <drinkjuice@naver.com>
2024-01-08 21:24:09 +0900

    add BottomNavigationBar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -12,6 +13,44 @@ class HomePage extends StatelessWidget {
       body: Column(
         children: [
           Text('hi'),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: 0,
+        showSelectedLabels: true,
+        selectedItemColor: Colors.black,
+        onTap: (index) {
+          print(index);
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset("assets/svg/icons/home_off.svg"),
+            activeIcon: SvgPicture.asset("assets/svg/icons/home_on.svg"),
+            label: "홈",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset("assets/svg/icons/compass_off.svg", width: 22,),
+            activeIcon: SvgPicture.asset("assets/svg/icons/compass_on.svg", width: 22,),
+            label: "탐색",
+          ),
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: SvgPicture.asset("assets/svg/icons/plus.svg", width: 35,),
+            ),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset("assets/svg/icons/subs_off.svg"),
+            activeIcon: SvgPicture.asset("assets/svg/icons/subs_on.svg"),
+            label: "구독",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset("assets/svg/icons/library_off.svg"),
+            activeIcon: SvgPicture.asset("assets/svg/icons/library_on.svg"),
+            label: "보관함",
+          )
         ],
       ),
     );
